ce57c33afa596eb644ce8afc127530c629e279cb
ogulenko-valentin <ogulenkov@inbox.ru>
2017-03-04 15:48:06 +0300

    Remake archetype

---
+++ src/main/java/dao/JdbcDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+/**
+ * Created by qwerty on 04.03.2017.
+ */
+public interface JdbcDAO<T> {
+    void setDataSource(DataSource dataSource);
+
+    List list();
+
+    void delete(Integer id);
+
+    void saveOrUpdate(T obj);
+
+    T get(Integer id);
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,106 @@
+package model;
+
+/**
+ * Created by qwerty on 04.03.2017.
+ */
+public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String birthday;
+    private Integer cityID;
+    private String password;
+    private String email;
+    private Integer gender;
+    private String info;
+    private Integer roleID;
+    private String userPhoto;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(Integer cityID) {
+        this.cityID = cityID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public Integer getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(Integer roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(String userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+}
